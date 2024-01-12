8b9135516a26e8fe7f240e93b74bf95923f5c196
poongwin <36015043+poongwin1@users.noreply.github.com>
2024-01-12 04:30:47 +0000

    whoopsies!! :P

--- src/Configs/config.js
+++ src/Configs/config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 module.exports = {
   GUILD_ID: "770320230810910741",
   BOT_ID: "1191418991911120957",
-  TOKEN: "MTE5MTQxODk5MTkxMTEyMDk1Nw.G8msVP.IBB-MQWP7K_NZ0o7e_CJuLoEsFeuDErNOrswxc",
+  TOKEN: "TOKEN",
 };
